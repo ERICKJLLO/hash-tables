d55d5d4d6b49cba21a91594d1edf9aa1faf18aff
Erickjllo <erickjllo07@gmail.com>
2025-04-27 21:04:54 -0500

    implementacion metodos y pruebas

--- hash_table.py
+++ hash_table.py
@@ -1,36 +1,122 @@
-class HashTable:
-    def __init__(self, size = 10 ):
-        self.size = size
-        self.table = [[] for _ in range(self.size)]
+from linked_lists import SinglyLinkedList
 
-    def _hash_knuth(self, key):
+class TablaHash:
+    def __init__(self, tamaño=10):
+        
+        self.tamaño = tamaño
+        self.tabla = [SinglyLinkedList() for _ in range(self.tamaño)]
+
+    def _hash_knuth(self, clave):
+        
         constante_aurea = 0.6180339887
-        return int(self.size * ((hash(key) * constante_aurea) % 1))
+        return int(self.tamaño * ((hash(clave) * constante_aurea) % 1))
     
-    def _hash_xor(self, key):
-        hash_value = 0
-        for char in str(key):
-            hash_value ^= (hash_value << 5) + (hash_value >>2) + ord(char)
-        return hash_value % self.size
+    def _hash_xor(self, clave):
+        
+        valor_hash = 0
+        
+        for caracter in str(clave):
+            
+            valor_hash ^= (valor_hash << 5) + (valor_hash >> 2) + ord(caracter)
+            
+        return valor_hash % self.tamaño
     
-    def _hash_personalizada(self, key):
-        hash_value = sum(ord(char) for char in str(key))
-        return (hash_value * 31) % self.size
+    def _hash_personalizada(self, clave):
+        
+        valor_hash = 0
+        
+        for caracter in str(clave):
+            valor_hash += ord(caracter)
+        return (valor_hash * 31) % self.tamaño
     
-    def _hash(self, key, metodo):
+    def _hash(self, clave, metodo):
+        
         if metodo == "knuth":
-            return self._hash_knuth(key)
+            return self._hash_knuth(clave)
+        
         elif metodo == "xor":
-            return self._hash_xor(key)
+            return self._hash_xor(clave)
+        
         elif metodo == "personalizada":
-            return self._hash_personalizada(key)
+            return self._hash_personalizada(clave)
 
+    def insert(self, clave, valor, metodo="knuth"):
+        
+        indice = self._hash(clave, metodo)
+        
+        for nodo in self.tabla[indice]:
+            if nodo[0] == clave:
+                nodo[1] = valor
+                return
+            
+        self.tabla[indice].insert([clave, valor])
 
-    def insert(self, key, value):
-       pass
-    def search(self, key):
-        pass
-    def delete(key):
-        pass
-    def __str__():
-        pass
+    def search(self, clave, metodo="knuth"):
+        
+        indice = self._hash(clave, metodo)
+        
+        for nodo in self.tabla[indice]:
+            if nodo[0] == clave:
+                return nodo[1]
+        return None
+
+    def delete(self, clave, metodo="knuth"):
+        
+        indice = self._hash(clave, metodo)
+        
+        for nodo in self.tabla[indice]:
+            
+            if nodo[0] == clave:
+                self.tabla[indice].delete(nodo)
+                return
+            
+        raise KeyError(f"La clave '{clave}' no fue encontrada")
+
+    def __str__(self):
+        return str([list(cubeta) for cubeta in self.tabla])
+
+def encontrar_duplicados(lista):
+    
+    tabla_hash = TablaHash()
+    duplicados = []
+    
+    for elemento in lista:
+        
+        if tabla_hash.search(elemento) is not None:
+            if elemento not in duplicados:
+                duplicados.append(elemento)
+    
+        else:
+            tabla_hash.insert(elemento, True)
+            
+    return duplicados
+
+def contar_elementos(lista):
+    
+    tabla_hash = TablaHash()
+    
+    for elemento in lista:
+        
+        frecuencia = tabla_hash.search(elemento)
+        
+        if frecuencia is None:
+            tabla_hash.insert(elemento, 1)
+            
+        else:
+            tabla_hash.insert(elemento, frecuencia + 1)
+    resultado = {}
+    
+    for lista_enlazada in tabla_hash.tabla:
+        
+        for clave, valor in lista_enlazada:
+            resultado[clave] = valor
+            
+    return resultado
+
+if __name__ == "__main__":
+    
+    lista_duplicados = ["a", "b", "c", "a", "b", "d"]   
+    print("Duplicados en la lista:", encontrar_duplicados(lista_duplicados))
+    
+    lista_frecuencia = ["a", "b", "c", "a", "b", "d"]
+    print("Frecuencia de elementos:", contar_elementos(lista_frecuencia))
